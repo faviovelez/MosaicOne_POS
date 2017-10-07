2b19987ccc823742349c2cb7bee8ef99d1bdfd5e
Oscar Hugo Cardenas Lopez <oscardev@cacyberlo.com.mx>
2017-10-07 18:45:15 -0500

    init electron application

---
+++ main.js
@@ -0,0 +1,16 @@
+'use strict';
+
+const electron = require('electron');
+const app = electron.app;
+const BrowserWindow = electron.BrowserWindow;
+let mainWindow = null;
+
+app.on('window-all-closed',() => {
+   if (process.platform !== 'darwin') app.quit();
+});
+
+app.on('ready', () => {
+   mainWindow = new BrowserWindow();
+   mainWindow.loadURL(`file://${app.getAppPath()}/app/views/sign_in.html`);
+   mainWindow.on('closed', () => { mainWindow = null; });
+});
