253f21f774f0b04679663c3403807be53dd252a7
Oscar Hugo Cardenas Lopez <oscardev@cacyberlo.com.mx>
2017-11-16 19:46:45 -0600

    temp commit

--- app/assets/js/openRegister.js
+++ app/assets/js/openRegister.js
@@ -1,6 +1,36 @@
 $(function(){
   const remote = require('electron').remote;
 
+  async function initStore(){
+
+    const store = new Store({
+      configName: 'user-localStore',
+      defaults: {
+        windowBounds: { width: 1024, height: 768 }
+      }
+    });
+
+    return store;
+  }
+
+  (function (){
+    initStore().then(store => {
+
+      let storeInfo  = store.get('store'),
+          localQuery = 'SELECT * FROM business_units WHERE ' +
+              `id = ${store.business_unit_id}`,
+          query      = {};
+      query(localQuery).then(business_unit => {
+
+        queries.billing_addresses = 'SELECT * FROM billing_addresses ' +
+          `WHERE id = ${mainResult.rows[0].billing_address_id}`;
+
+
+      });
+
+    });
+  })();
+
   $('#datetimepicker1 #datetimepicker2').datetimepicker({
     locale: 'es'
   });
