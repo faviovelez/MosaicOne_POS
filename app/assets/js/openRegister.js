5acdef865ef39e4be2eba0148bc2f4934060be34
Oscar Hugo Cardenas Lopez <oscardev@cacyberlo.com.mx>
2017-11-17 00:01:09 -0600

    clean database billing adress table

--- app/assets/js/openRegister.js
+++ app/assets/js/openRegister.js
@@ -17,14 +17,29 @@ $(function(){
     initStore().then(store => {
 
       let storeInfo  = store.get('store'),
-          localQuery = 'SELECT * FROM business_units WHERE ' +
-              `id = ${store.business_unit_id}`,
-          query      = {};
-      query(localQuery).then(business_unit => {
+        localQuery = 'SELECT * FROM business_units WHERE ' +
+        `id = ${storeInfo.business_unit_id}`;
 
-        queries.billing_addresses = 'SELECT * FROM billing_addresses ' +
-          `WHERE id = ${mainResult.rows[0].billing_address_id}`;
+      ids = [];
 
+      query(localQuery, false).then(business_unit => {
+
+        ids.push(business_unit.rows[0].billing_address_id);
+
+        let localQuery = 'SELECT billing_address_id FROM prospects' +
+          ` WHERE billing_address_id IS NOT NULL`;
+
+        query(localQuery, false).then(prospects => {
+
+          prospects.rows.forEach(row => {
+            ids.push(row.billing_address_id);
+          });
+
+          let finalQuery = 'DELETE FROM billing_addresses' +
+                           ` WHERE id NOT IN (${ids})`;
+
+          query(finalQuery, false).then(() => {});
+        });
 
       });
 
