067292c66dbcf950dc2e3e146871d7ed1b7651e9
Favio Velez <favio.velez@hotmail.com>
2018-02-03 20:02:08 -0600

    changes to print

--- app/assets/js/reimpresion.js
+++ app/assets/js/reimpresion.js
@@ -3,7 +3,7 @@ let localWin = null;
 
 function reimpresion(ticketId) {
   localWin = new localRemote.BrowserWindow({width: 800, height: 600, show: false });
-  let path = `../../../tickets/TicketNo_${ticketId}.html)`;
+  let path = `./tickets/TicketNo_${ticketId}.html`;
   localWin.loadURL(`file://${path}`);
 
   let contents = localWin.webContents;
